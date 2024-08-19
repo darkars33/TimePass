0a8f2d1e819f210bdf8cd5c0e9bbbb778da54e1a
darkars33 <darshangarad18@gmail.com>
2024-08-19 14:26:57 +0530

    reduc initialize

--- frontend/src/components/Filter.tsx
+++ frontend/src/components/Filter.tsx
@@ -1,9 +1,12 @@
-
+import { IoIosSearch } from "react-icons/io";
 
 const Filter = () => {
   return (
-    <div>
-      Filter
+    <div className="relative w-[20rem] flex items-center">
+      <input type="text" placeholder="Search" className="w-full p-2 border bg-gray-200 rounded-md focus:outline-none" />
+      <div className="absolute right-5">
+          <IoIosSearch className="text-2xl text-gray-500" />
+      </div>
     </div>
   )
 }
--- frontend/src/components/LogOut.tsx
+++ frontend/src/components/LogOut.tsx
@@ -2,8 +2,14 @@
 
 const LogOut = () => {
   return (
-    <div>
-      logout
+    <div className="flex justify-center items-center gap-4">
+      <div className="w-11 h-11 bg-gray-300 flex justify-center items-center rounded-full">
+          <h1>TU</h1>
+      </div>
+      <div>
+          <h1 className="text-sm font-semibold">Test User</h1>
+          <button className="underline text-gray-400 hover:text-black">LogOut</button>
+      </div>
     </div>
   )
 }
